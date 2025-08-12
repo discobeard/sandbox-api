4a86152d38bd4f3060336c76b31b5f7cb032e0e2
discobeard <mcgill.damian@gmail.com>
2025-08-12 19:09:22 +0100

    initial boiler plate commit

---
+++ lib/lambdas/hello-world.ts
@@ -0,0 +1,12 @@
+import { APIGatewayProxyEvent } from 'aws-lambda';
+
+
+export const handler = async (event: APIGatewayProxyEvent) => {
+    const response: string = 'The quick brown fox jumps over the lazy dog';
+    return {
+        statusCode: 200,
+        body: {
+            message: `Hello world - ${response}`,
+        }
+    }
+}
--- lib/sandbox-api-stack.ts
+++ lib/sandbox-api-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import {NodejsFunction} from "aws-cdk-lib/aws-lambda-nodejs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class SandboxApiStack extends cdk.Stack {
@@ -8,6 +9,9 @@ export class SandboxApiStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
+    const helloWorldLambda = new NodejsFunction(this, 'helloWorldLambda',{
+
+    });
     // example resource
     // const queue = new sqs.Queue(this, 'SandboxApiQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
