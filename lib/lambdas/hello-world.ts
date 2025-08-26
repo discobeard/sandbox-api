e168180c686bb9aca893bb384a1eed577019987d
discobeard <mcgill.damian@gmail.com>
2025-08-26 17:57:12 +0100

    add placeholders for new lambdas and table

--- lib/lambdas/hello-world.ts
+++ lib/lambdas/hello-world.ts
@@ -1,12 +1,10 @@
-
-
 export const handler = async () => {
-        const response: string = 'The quick brown fox jumps over the lazy dog';
-        console.log('Hello cloud watch!')
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-                message: `Hello world - ${response} ${process.env.HELLO_WORLD || ''}`,
-            }),
-        };
+    const response: string = 'The quick brown fox jumps over the lazy dog';
+    console.log('Hello cloud watch!')
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: `Hello world - ${response} ${process.env.MY_TEST_SECRET || ''}`,
+        }),
+    };
 }
--- lib/sandbox-api-stack.ts
+++ lib/sandbox-api-stack.ts
@@ -1,14 +1,19 @@
 import * as cdk from 'aws-cdk-lib';
+import {RemovalPolicy} from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apiGateway from 'aws-cdk-lib/aws-apigateway';
 import {Construct} from 'constructs';
 import {NodejsFunction} from "aws-cdk-lib/aws-lambda-nodejs";
+import {AttributeType, Billing, TableV2} from "aws-cdk-lib/aws-dynamodb";
 
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+// TODO: Create a dynamoDB table that will store restuarant IDs as the partition key, with the restaurant name and rating as additional details.
+
+// TODO: create lambdas that will retrieve and update the table with restaurant information
 
 export class SandboxApiStack extends cdk.Stack {
     // Declare lambda functions to be created here.
     public readonly helloWorldLambda: NodejsFunction;
+    public readonly restaurantTable: TableV2;
 
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
@@ -18,23 +23,35 @@ export class SandboxApiStack extends cdk.Stack {
             restApiName: 'Sanbox Api'
         })
 
-        // Create nodejsfunctions  to be assigned to the lambbdas here
+        // Create nodejsfunctions  to be assigned to the lambdas here
         this.helloWorldLambda = new NodejsFunction(this, 'helloWorldLambda', {
+            functionName: 'hello-world',
             entry: 'lib/lambdas/hello-world.ts',
             handler: 'handler',
             runtime: lambda.Runtime.NODEJS_22_X,
             environment: {
-                HELLO_WORLD: "The cat stalks the mouse"
+                HELLO_WORLD: "The cat stalks the mouse",
+                MY_TEST_SECRET: process.env.MY_TEST_SECRET || ''
             },
             memorySize: 128,
             timeout: cdk.Duration.seconds(5),
         });
 
+        this.restaurantTable = new TableV2(this, 'RestaurantTable', {
+            partitionKey: {
+                name: 'restaurantId',
+                type: AttributeType.STRING,
+            },
+            tableName: 'restaurants',
+            billing: Billing.onDemand(),
+            removalPolicy: RemovalPolicy.DESTROY,
+        })
+
         // create routes here and assign lambda functions to them.
 
         // Hello world lambda - route
         const helloWorldRoute = apiGw.root.addResource('hello');
-        helloWorldRoute.addMethod('GET', new apiGateway.LambdaIntegration(helloWorldLambda));
+        helloWorldRoute.addMethod('GET', new apiGateway.LambdaIntegration(this.helloWorldLambda));
 
     }
 }
